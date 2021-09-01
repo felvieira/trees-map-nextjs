b521083a25d3442a10716f48bcd1673529d1da0f
felvieira <flpchapola@hotmail.com>
2021-09-01 18:56:17 -0300

    Alterado title da pagina

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,14 +15,11 @@ function App({ Component, pageProps }: AppProps) {
 		<Provider>
 			<ThemeProvider theme={theme}>
 				<Head>
-					<title>React Next- Boilerplate</title>
+					<title>Trees Map - React with Next.js</title>
 					<link rel="shortcut icon" href="/img/icon-512.png" />
 					<link rel="apple-touch-icon" href="/img/icon-512.png" />
 					<link rel="manifest" href="/manifest.json" />
-					<meta
-						name="description"
-						content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-					/>
+					<meta name="description" content="Trees Map - React with Next.js" />
 				</Head>
 				<GlobalStyles />
 				<Component {...pageProps} />
